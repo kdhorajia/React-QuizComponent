08f527355325d69b44c7c4a3c1f5fc378ff6e97a
Khushbu Dhorajia <khushbu_dhorajia@yahoo.com>
2020-01-26 20:09:54 -0500

    chapter 6 changes

--- src/Quiz.js
+++ src/Quiz.js
@@ -17,7 +17,7 @@ class Quiz extends Component {
         return (
             <div>
                 {isQuizEnd ? <QuizEnd /> :
-                    <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandle={this.showNextQuestion.bind(this)} />}
+                    <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)} />}
             </div>
         );
     }
